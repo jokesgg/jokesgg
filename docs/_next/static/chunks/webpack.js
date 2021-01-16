cccca95678a900080bb64952a5433233e990fe2c
lacksfish <lacksfish@gmail.com>
2021-01-16 06:58:05 +0100

    update

--- docs/_next/static/chunks/webpack.js
+++ docs/_next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "44d3cc5eb324d8a1f887";
+/******/ 	var hotCurrentHash = "b158b0f7a9d3851a6f80";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
